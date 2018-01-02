13518a1176748a1e80995a2577ec5c541083fba0
defuera <vdenis@zvooq.com>
2018-01-02 23:13:28 +0100

    reconnect websocket with timeout

--- app/src/main/java/ru/justd/bux/app/model/WebsocketApiImpl.kt
+++ app/src/main/java/ru/justd/bux/app/model/WebsocketApiImpl.kt
@@ -1,5 +1,6 @@
 package ru.justd.bux.app.model
 
+import android.os.Handler
 import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -7,8 +8,10 @@ import okhttp3.*
 import ru.justd.bux.BuildConfig
 import rx.Observable
 import rx.subjects.PublishSubject
+import java.util.*
 
 private const val TAG = "WebsocketApiImpl"
+private const val DELAY_RECONNECT = 5000L
 
 class WebsocketApiImpl constructor(
         private val okHttpClient: OkHttpClient
@@ -17,19 +20,26 @@ class WebsocketApiImpl constructor(
     private val gson = Gson()
     private val subject = PublishSubject.create<TradingQuote>()
     private lateinit var webSocket: WebSocket
-    
+
     override fun connect() {
         webSocket = okHttpClient.newWebSocket(
                 Request.Builder().url("${BuildConfig.WS_HOST}subscriptions/me").build(),
                 object : WebSocketListener() {
                     override fun onOpen(webSocket: WebSocket, response: Response) {
-                        Log.d(TAG, ": ");
+                        Log.d(TAG, "onOpen")
                     }
 
                     override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
                         subject.onError(t)
                         webSocket.cancel()
-                        connect()
+                        Timer().schedule(
+                                object : java.util.TimerTask() {
+                                    override fun run() {
+                                        connect()
+                                    }
+                                },
+                                DELAY_RECONNECT
+                        ) //yeah, it would be nice to check if there's network availavle before actually tryint to reconnect
                     }
 
                     override fun onMessage(webSocket: WebSocket?, text: String) {
@@ -51,10 +61,10 @@ class WebsocketApiImpl constructor(
 
     override fun observeProduct(productId: String): Observable<TradingQuote> =
             subject
-                .doOnSubscribe {
-                    Log.i(TAG, "subscribe to product: $productId")
-                    webSocket.send("""{"subscribeTo":["trading.product.$productId"]}""")
-                }
+                    .doOnSubscribe {
+                        Log.i(TAG, "subscribe to product: $productId")
+                        webSocket.send("""{"subscribeTo":["trading.product.$productId"]}""")
+                    }
 
     override fun unsubscribe(productId: String) {
         Log.i(TAG, "unsubscribe from product: $productId");
